37ce97e86d455c38517aca0ac1c393bcf66b8e71
Denise Williams <williamsde@outlook.com>
2016-07-28 10:39:53 -0400

    Homework Attempt

---
+++ 8ball.js
@@ -0,0 +1,21 @@
+﻿alert("Hello"); //sanity check
+function figureIt()
+{
+    //randomly pick phrase
+    //display it
+    //alert("Figure It ");
+    var myTargetParagraph = document.getElementById('fortune');
+    //way to refer to paragraph element
+    var randomFortune = [
+        "You've know the answer all the time",
+        'No, but maybe',
+        'How should I know',
+        'What do you think',
+        'Probably, but highly unlikely'
+    ]; //0-8 Index need to created to contain all fortunes to choose from
+
+    var randomNumber = Math.floor(Math.random() * randomFortune.length);
+    //create an array to hold the differnt fortunes
+    myTargetParagraph.innerHTML = randomFortune[randomNumber];
+}
+
